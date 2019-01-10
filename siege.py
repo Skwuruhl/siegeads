590e9375d8fb1359fb0e5ce5ee14db6c2d22280c
Skwuruhl <smmarion11@gmail.com>
2019-01-10 06:33:35 -0800

    added rough support for glaz's scope

--- siege.py
+++ siege.py
@@ -8,9 +8,12 @@ coef = float(input("Coefficient="))
 if mode == 'zoom':
     acog = math.tan(math.radians(fov)/2*0.35)/math.tan(math.radians(fov)/2)/0.35 * coef
     iron = math.tan(math.radians(fov)/2*0.9)/math.tan(math.radians(fov)/2)/.6 * coef
+    glaz = math.tan(math.radians(fov)/2*0.3)/math.tan(math.radians(fov)/2)/0.3 * coef
 else:
     acog = math.atan(coef*math.tan(math.radians(fov)/2*0.35))/math.atan(coef*math.tan(math.radians(fov)/2))/0.35
     iron = math.atan(coef*math.tan(math.radians(fov)/2*0.9))/math.atan(coef*math.tan(math.radians(fov)/2))/0.6
+    glaz = math.atan(coef*math.tan(math.radians(fov)/2*0.3))/math.atan(coef*math.tan(math.radians(fov)/2))/0.3
+
 
 approximations = str(Fraction(acog/iron).limit_denominator(100)).split('/')
 
@@ -21,5 +24,7 @@ XFactor = acog / approximations[0]
 output = '\nACOG ADS Sensitivity: {}\nIronsight ADS Sensitivity: {}'.format(approximations[0], approximations[1])
 
 output += '\nXFactorAiming={:.6f}'.format(XFactor)
+
+output += '\nGlaz Scope Sensitivity: {}'.format(round(glaz/XFactor))
 
 print(output)
